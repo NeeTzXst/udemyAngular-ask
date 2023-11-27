5aa9b9ab56076b5cc1203ddb70969129da03f722
Pattima Sommai <ppp.pattima@hotmail.com>
2023-11-27 16:26:07 +0700

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { RouterOutlet } from '@angular/router';
+declare const App: any;
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true
 })
-export class AppComponent {
-  title = 'installTest';
+
+export class AppComponent implements OnInit {
+  title = 'app';
+  ngOnInit() {
+    App.initialLoadPage();
+  }
 }
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [CommonModule, RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   title = 'installTest';
+// }
